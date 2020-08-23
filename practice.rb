43299fde4790806379c7716bbd399d95d0763ed6
Serkan Sahin <ssahin55@outlook.com>
2020-08-23 16:43:06 +0000

    Done.

---
+++ practice.rb
@@ -0,0 +1,29 @@
+# director_1 = {
+#   name: "Ida Lupino",
+#   favorite_movie: "The Hitch-Hiker"
+# }
+
+# director_2 = {
+#   name: "Orson Welles",
+#   favorite_movie: "Citizen Kane"
+# }
+
+# director_3 = {
+#   name: "Andrej Tarkovsky",
+#   favorite_movie: "Solaris"
+# }
+
+# p film_fest = [
+#   director_1,
+#   director_2,
+#   director_3
+#   ]
+
+directors = [
+  {:name=>"Ida Lupino", :favorite_movie=>"The Hitch-Hiker"},
+  {:name=>"Orson Welles", :favorite_movie=>"Citizen Kane"},
+  {:name=>"Andrej Tarkovsky", :favorite_movie=>"Solaris"}
+]
+
+p directors[0]
+# p directors[0][:name]
